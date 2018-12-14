bc2eea006a46a7e26c1178f69dbf613c94493605
Scott Hullinger <sbhullinger@gmail.com>
2018-12-14 15:24:58 -0800

    added various php files for catalog presentation

--- catalog.php
+++ catalog.php
@@ -23,13 +23,18 @@ include("inc/header.php"); ?>
 
     <div class="wrapper">
 
-        <h1><?php echo $pageTitle; ?></h1>
+        <h1><?php 
+        if ($section != null) {
+            echo "<a href='catalog.php'>Full Catalog</a> &gt; ";
+        }   
+        echo $pageTitle; ?></h1>
 
         <ul class="items">
             <?php
-            foreach ($catalog as $id => $item) {
-                echo get_item_html($id,$item);
-            } 
+            $categories = array_category($catalog,$section);
+            foreach ($categories as $id) {
+                echo get_item_html($id,$catalog[$id]);
+            }
             ?>
         </ul>
     </div>
--- inc/functions.php
+++ inc/functions.php
@@ -7,3 +7,21 @@ function get_item_html($id,$item) {
         . "</a></li>";
     return $output;
 }
+
+function array_category($catalog,$category) {
+    $output = array();
+
+    foreach ($catalog as $id => $item) {
+        if ($category == null OR  strtolower($category) == strtolower($item["category"])) {
+            $sort = $item["title"];
+            $sort = ltrim($sort, "The "); //removes the, a & a when sorting
+            $sort = ltrim($sort, "A ");
+            $sort = ltrim($sort, "An ");
+            $output[$id] = $sort;
+        }
+    }
+
+    asort($output);
+    return array_keys($output);
+
+}
