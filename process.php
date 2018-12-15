bd9bfe346210d2a262a4d6f35068892aafacdcf1
Scott Hullinger <sbhullinger@gmail.com>
2018-12-15 11:11:59 -0800

    added process.php to process web form

---
+++ process.php
@@ -0,0 +1,11 @@
+<?php
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$details = $_POST["details"];
+
+echo "Name " . $name . "\n";
+echo "Email " . $email . "\n";
+echo "Details " . $details . "\n";
+
+?>
--- suggest.php
+++ suggest.php
@@ -5,7 +5,29 @@ $section = "suggest";
 include("inc/header.php"); ?>
 
 <div class="section page">
-    <h1>Suggest a Media Item</h1>
+    <div class="wrapper">
+        <h1>Suggest a Media Item</h1>
+        <p>If you think there is something I&rsquo;m missing, let me know! Complete the form to send me an email.</p>
+        <form method="post" action="process.php">
+            <table>
+                <tr>
+                    <th><label for="name">Name</label></th>
+                    <td><input type="text" id="name" name="name" /></td>
+                </tr>
+                <tr>
+                    <th><label for="email">Email</label></th>
+                    <td><input type="text" id="email" name="email" /></td>
+                </tr>
+                <tr>
+                    <th><label for="details">Suggest Item Details</label></th>
+                    <td><textarea name="details" id="details"></textarea></td>
+                </tr>
+            </table>
+            <input type="submit" value="send" />
+            
+        </form>
+    </div>
+
 </div>
 
 <?php include("inc/footer.php"); ?>
